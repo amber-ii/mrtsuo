5ffe4ae46e584d4a609431be010b200d9df8cc87
amber-ii <lube8262@gmail.com>
2020-10-03 01:26:08 +0800

    框架配置

--- patisserie/src/main/java/com/mrtsuo/web/IndexController.java
+++ patisserie/src/main/java/com/mrtsuo/web/IndexController.java
@@ -1,9 +1,10 @@
 package com.mrtsuo.web;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mrtsuo.NotFoundException;
 
 /**
  * 
@@ -12,13 +13,13 @@ import com.mrtsuo.NotFoundException;
  */
 @Controller
 public class IndexController {
-	@RequestMapping("/index")
-	public String index() {
+	@RequestMapping("/{id}/{name}")
+	public String index(@PathVariable Integer id, @PathVariable String name) {
 //		模擬一個文章或是商品找不到，	再自定義一個異常類，不要直接導入到404
-		String patisserie = null;
-		if(patisserie == null) {
-			throw new NotFoundException("商品不存在");
-		}
+//		String patisserie = null;
+//		if(patisserie == null) {
+//			throw new NotFoundException("商品不存在");
+//		}
 		return "index";
 	}
 }
