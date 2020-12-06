274e4426440ea6fd10e7a3bfd141274c8e682bee
amber-ii <lube8262@gmail.com>
2020-12-06 14:25:47 +0800

    original

--- patisserie/src/main/java/com/mrtsuo/PatisserieApplication.java
+++ patisserie/src/main/java/com/mrtsuo/PatisserieApplication.java
@@ -1,21 +1,11 @@
 package com.mrtsuo;
 
-import java.time.Instant;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class PatisserieApplication {
-	@GetMapping("/time")
-    public String getCurrentTime() {
-
-        return Instant.now().toString();
-    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PatisserieApplication.class, args);
 	}
